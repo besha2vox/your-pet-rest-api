ffda101077cd8cfe2af8c41811a15f7b34bf3b46
ASyrotenko <andreysyrotenko@gmail.com>
2023-05-11 16:57:45 +0300

    fix-response-v2

--- controllers/news.js
+++ controllers/news.js
@@ -3,12 +3,12 @@ const { News } = require("../models");
 const getAllNews = async (req, res) => {
   const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
-  const data = await News.find({}, "", {
+  const result = await News.find({}, "", {
     skip,
     limit: Number(limit),
   });
   res.json({
-    data,
+    result,
   });
 };
 
