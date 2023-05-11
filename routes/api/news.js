a1435ef93d7a21780f2fff030e080d8652e25557
ASyrotenko <andreysyrotenko@gmail.com>
2023-05-11 11:54:18 +0300

    test-news-route

--- routes/api/news.js
+++ routes/api/news.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
-// Закоментував тому, що поки змінна ніде не використана - eslint видає помилку
-// const { news: ctrl } = require("../../controllers");
+const { news: ctrl } = require("../../controllers");
 
 const router = express.Router();
+
+router.get("/", ctrl.getAllNews);
 
 module.exports = router;
