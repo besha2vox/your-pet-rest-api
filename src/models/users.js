4dfeb23914cdf81cedbf99c759d4e923c24bc9cf
TetianaKuzma <tetiana.b97@gmail.com>
2023-05-11 21:54:32 +0300

    Merge changes from main branch

---
+++ src/models/users.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleMongooseError } = require("../utils/validation/index");
+
+const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegex,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 8,
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+    avatarURL: {
+      type: String,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const schemas = {
+  // Create new user schema
+  registerSchema: Joi.object({
+    username: Joi.string().required(),
+    email: Joi.string().pattern(emailRegex).required(),
+    password: Joi.string().min(8).required(),
+  }),
+  // Log in with a user
+  loginSchema: Joi.object({
+    email: Joi.string().pattern(emailRegex).required(),
+    password: Joi.string().min(8).required(),
+  }),
+};
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemas };
