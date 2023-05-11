40c48e5b41b1ca9d3fe1513b7b760d3a4ea825c4
ASyrotenko <andreysyrotenko@gmail.com>
2023-05-11 16:37:59 +0300

    change response format

--- controllers/news.js
+++ controllers/news.js
@@ -3,14 +3,12 @@ const { News } = require("../models");
 const getAllNews = async (req, res) => {
   const { page = 1, limit = 6 } = req.query;
   const skip = (page - 1) * limit;
-  const result = await News.find({}, "", {
+  const data = await News.find({}, "", {
     skip,
     limit: Number(limit),
   });
   res.json({
-    status: "success",
-    code: 200,
-    data: { result },
+    data,
   });
 };
 
