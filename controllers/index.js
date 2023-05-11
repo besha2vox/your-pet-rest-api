fee9af4c9e5e058803fe00bd352633a1db8ae575
ASyrotenko <andreysyrotenko@gmail.com>
2023-05-11 11:30:05 +0300

    add-base-controllers

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const users = require("./users");
+const notices = require("./notices");
+const pets = require("./pets");
+const news = require("./news");
+const ourFriends = require("./ourFriends");
+
+module.exports = { users, notices, pets, news, ourFriends };
--- routes/api/news.js
+++ routes/api/news.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+// Закоментував тому, що поки змінна ніде не використана - eslint видає помилку
+// const { news: ctrl } = require("../../controllers");
+
 const router = express.Router();
 
 module.exports = router;
