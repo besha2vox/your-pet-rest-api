15741aa980be8e6067f7d0e1c802bcd84a0af8c7
ASyrotenko <andreysyrotenko@gmail.com>
2023-05-11 16:11:23 +0300

    getAllNews-add-pagination

--- controllers/news.js
+++ controllers/news.js
@@ -1,7 +1,12 @@
 const { News } = require("../models");
 
 const getAllNews = async (req, res) => {
-  const result = await News.find();
+  const { page = 1, limit = 6 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await News.find({}, "", {
+    skip,
+    limit: Number(limit),
+  });
   res.json({
     status: "success",
     code: 200,
