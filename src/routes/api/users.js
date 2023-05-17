c2cf90b9f10d71f41a101cacb8e9d7a7c235b773
Serhii Demchenko <svdemchenko@gmail.com>
2023-05-18 00:09:21 +0300

    add endpoints to swagger

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,48 +1,36 @@
-const express = require('express');
-const { validation, auth } = require('../../middlewares');
-const {
-    registerSchema,
-    loginSchema,
-    refreshSchema,
-    updateUserSchema,
-} = require('../../middlewares');
-const { schemas, updatePetJoiSchema } = require('../../db/models');
-const { users: ctrl } = require('../../controllers');
+const express = require("express");
+const { validation, auth } = require("../../middlewares");
+const { registerSchema, loginSchema, refreshSchema, updateUserSchema } = require("../../middlewares");
+const { updatePetJoiSchema } = require("../../db/models");
+const { users: ctrl } = require("../../controllers");
 
 const router = express.Router();
 
 // Register
-router.post('/register', validation(registerSchema), ctrl.register);
+router.post("/register", validation(registerSchema), ctrl.register);
 
 // Log in
-router.post('/login', validation(loginSchema), ctrl.login);
+router.post("/login", validation(loginSchema), ctrl.login);
 
 // Refresh
-router.post('/refresh', validation(refreshSchema), ctrl.refresh);
+router.post("/refresh", validation(refreshSchema), ctrl.refresh);
 
 // Get current User
-router.get('/current', auth, ctrl.getCurrent);
+router.get("/current", auth, ctrl.getCurrent);
 
 // Log out
-router.post('/logout', auth, ctrl.logout);
+router.post("/logout", auth, ctrl.logout);
 
 // Verification
-router.get('/verify/:verificationToken', ctrl.verify);
+router.get("/verify/:verificationToken", ctrl.verify);
 
 // Update User
-router.put('/:id', auth, validation(updateUserSchema), ctrl.updateUser);
+router.put("/:id", auth, validation(updateUserSchema), ctrl.updateUser);
 
 // Get info about user and user's pets
-router.get('/:id', auth, ctrl.getUserInfo);
+router.get("/:id", auth, ctrl.getUserInfo);
 
-// Update info about user and user's pets
-// router.patch('/:id', auth, validation(updateUserSchema), ctrl.updateUserInfo);
-
-router.put(
-    '/pets/:id',
-    auth,
-    validation(updatePetJoiSchema),
-    ctrl.updateUserPets
-);
+// Update and user's pets
+router.put("/pets/:id", auth, validation(updatePetJoiSchema), ctrl.updateUserPets);
 
 module.exports = router;
